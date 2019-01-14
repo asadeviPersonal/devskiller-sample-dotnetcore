44fd81ff52f3ddbe956b26bb6cdbe31ada936919
Maciej Halaczkiewicz <maciej.halaczkiewicz@gmail.com>
2019-01-14 18:47:02 +0100

    Initial commit

---
+++ CalculatorSample/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CalculatorSample
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            checked
+            {
+                return a + b;
+            }
+        }
+
+        public int Substract(int a, int b)
+        {
+            checked
+            {
+                return a - b;
+            }
+        }
+
+        public int Multiply(int a, int b)
+        {
+            checked
+            {
+                return a * b;
+            }
+        }
+
+        public double Divide(int a, int b)
+        {
+            checked
+            {
+                if(b==0)
+                    throw new DivideByZeroException();
+                var result = (double) a / b;
+                return result;
+            }
+        }
+    }
+}
